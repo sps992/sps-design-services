87c98975ed41068b2724e6a9242a5fd859622c7c
Sam <hello@samsedgeman.co.uk>
2020-03-27 20:00:52 +0000

    Writing JS for contact form and adding script tag to base.html

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,13 +1,31 @@
+var contact = $("form#contact");
+contact.submit(function(event){
+	event.preventDefault();
+ 
+    (function(){
+      emailjs.init("user_GXxEk5JXUi6PanXg50NK5");
+   })();
+  // Change to your service ID, or keep using the default service
+  var service_id = "gmail";
+  var template_id = "contact_form";
 
- (function(){
-           emailjs.init('user_GXxEk5JXUi6PanXg50NK5');
-        })();
+ contact.find("button").text("Sending...");
+  emailjs.sendForm(service_id,template_id,contact[0])
+  	.then(function(){ 
+        $('#form-message').removeClass('hide').addClass('alert alert-success alert-dismissible').slideDown().show();
+        $('#messages_content').html('<p>Thank you for your message, I will be in touch ASAP!</p>');
+       contact.find("button").text("Send");
+    }, function(err) {
+        $('#messages').removeClass('hide').addClass('alert alert-success alert-dismissible').slideDown().show();
+        $('#messages_content').html('<p>Sorry, something has gone wrong. Please try again later or contact us on <span><a href="mailto:support@samsedgeman.co.uk">support@samsedgeman.co.uk</a></span></p>');
+       contact.find("button").text("Send");
+    });
+  return false;
+  
+});
 
-window.onload = function() {
-            document.getElementById('contact-form').addEventListener('submit', function(event) {
-                event.preventDefault();
-                // generate the contact number value
-                this.contact_number.value = Math.random() * 100000 | 0;
-                emailjs.sendForm('contact_service', 'contact_template', this);
-                    });
-        }
+function formTidy() {
+            document.contact-form.submit();
+            document.contact-form.reset();
+            }
+
